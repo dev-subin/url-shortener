c873b60224c1f23580578a524707664417360f0e
subin sabu <subinsabu148@gmail.com>
2022-02-28 13:51:06 +0530

    url-shortener css-responsive

--- src/Footer.js
+++ src/Footer.js
@@ -5,15 +5,15 @@ const Footer = () => {
     <div className="bg-[#0b1736]  bg-[url('https://url-shortner-app-react.netlify.app/assets/bg-boost-desktop.05593952.svg')] bg-cover bg-no-repeat mt-12">
        <div className='flex flex-col md:flex-row place-content-evenly'>
        <div className='pt-16 p-10 text-center md:text-left'>
-        <h1 className="font-mono text-white text-5xl">
-            Url <span className="text-orange-400 font-mono text-5xl">Shortener</span>
+        <h1 className="font-mono text-white text-3xl md:text-5xl">
+            Url <span className="text-orange-400 font-mono text-3xl md:text-5xl">Shortener</span>
           </h1>
           <div className='mt-10'>
-        <a href="https://www.linkedin.com/in/subin-sabu-b0b610224/"> <p className='text-white font-bold text-xl'> https://www.linkedin.com/in/subin-sabu-b0b610224/</p></a>
+        <a href="https://www.linkedin.com/in/subin-sabu-b0b610224/"> <p className='text-white font-bold text-lg md:text-xl'> https://www.linkedin.com/in/subin-sabu-b0b610224/</p></a>
           </div>
           <div className='md:mb-44'>
              
-             <a href="https://github.com/dev-subin"> <p className='font-bold text-white text-xl'> https://github.com/dev-subin</p></a>
+             <a href="https://github.com/dev-subin"> <p className='font-bold text-white text-lg md:text-xl'> https://github.com/dev-subin</p></a>
           </div>
         </div>
         <div className='md:mt-36 text-white font-bold text-center md:text-left'>
